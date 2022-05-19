345204600134abc1daf0cd08cd214d25a3fad7df
DanielGRasmussen <danielgrasmussen42@gmail.com>
2022-05-19 14:12:09 -0700

    Finished project.

--- lesson5/js/main.js
+++ lesson5/js/main.js
@@ -1,9 +1,19 @@
 const input = document.getElementById("favchap");
 const button = document.getElementById("submit");
 const list = document.getElementsByClassName("listcontainer");
+var li, remove;
 
 button.addEventListener("click", function() {
-    var li = document.createElement("li");
-    li.innerHTML = "This is a test"
+    li = document.createElement("li");
+    remove = document.createElement("button");
+
+    remove.innerHTML = "❌";
+    remove.setAttribute("onclick", "this.parentNode.remove(); input.focus();")
+
+    li.innerHTML = input.value;
+    li.appendChild(remove);
     list[0].appendChild(li);
+    
+    input.value = "";
+    input.focus();
 });
