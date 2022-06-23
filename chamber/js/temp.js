4953648014a304adb9674c4dccd39423ee197082
DanielGRasmussen <danielgrasmussen42@gmail.com>
2022-06-23 15:43:31 -0700

    Added weather api to main page.

---
+++ chamber/js/temp.js
@@ -0,0 +1,38 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const currentSpeed = document.querySelector('#windSpeed')
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=34.5134&lon=-110.0784&units=imperial&appid=ce994598a883d6400cf01fb5e0818308';
+
+function  displayResults(weatherData) {
+    currentTemp.innerHTML = weatherData.main.temp.toFixed(0);
+    currentSpeed.innerHTML = weatherData.wind.speed.toFixed(0);
+  
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+  
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc.replace(/\w\S*/g, function(txt){
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });;;
+}
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+apiFetch();
