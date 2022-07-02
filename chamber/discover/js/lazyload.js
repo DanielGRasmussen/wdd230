14d24d6216a4128df405b2d13c8627e2adc3c7d0
DanielGRasmussen <danielgrasmussen42@gmail.com>
2022-07-02 13:16:48 -0700

    Fixed lazy loading on discover page/made page weight below 500kb.

--- chamber/discover/js/lazyload.js
+++ chamber/discover/js/lazyload.js
@@ -1,26 +1,26 @@
-const imagesToLoad = document.querySelectorAll("source[data-srcset]");
-
-const imgOptions = {
-    threshold: 0, 
-    rootMargin: "0px 0px 50px 0px"
-};
+let imagesToLoad = document.querySelectorAll('img[data-src]');
 
 const loadImages = (image) => {
-    image.setAttribute("srcset", image.getAttribute("data-srcset"));
+    image.setAttribute('src', image.getAttribute('data-src'));
     image.onload = () => {
-        image.removeAttribute("data-srcset");
+        image.removeAttribute('data-src');
     };
 };
 
 if ("IntersectionObserver" in window) {
-    const imgObserver = new IntersectionObserver((items, observer) => {
+    const observer = new IntersectionObserver((items, observer) => {
         items.forEach((item) => {
-            loadImages(item.target);
-            observer.unobserve(item.target);
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
         });
-    }, imgOptions);
-
-    imagesToLoad.forEach((img) => {
-        imgObserver.observe(img);
     });
-} else{}
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
+    }); 
+} else {
+    imagesToLoad.forEach((img) => {
+    loadImages(img);
+    });
+}
